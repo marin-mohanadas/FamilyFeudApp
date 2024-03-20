f21db3ba1d5f94b0d93f1c89775c9b45feec6f52
Marin N Mohanadas <niro.marin@gmail.com>
2024-03-20 16:22:21 -0400

    Home file changes
    1. modify home file to function based components
    2. api call and retrieve data

--- FamilyFeudApp/ClientApp/src/components/Home.js
+++ FamilyFeudApp/ClientApp/src/components/Home.js
@@ -1,26 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export class Home extends Component {
-  static displayName = Home.name;
+const Home = () => {
+    const [state, setState] = useState({
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+        question: "",
+        answers: '',
+        rank = 0
+        loading: true
+
+    });
+
+    useEffect(() => {
+        const populateData = async () => {
+            const response = await fetch('/api/home');
+            const data = await response.json();
+
+            //console.log(data)
+            //data.forEach(x => {
+
+            //});
+
+            //Question:
+            const questionNum = 1 // this should be the input
+
+            const qus = data.find(x => x.questionNumber === questionNum).questions;
+            const answerAndRank = data.filter(x => x.questionNumber === questionNum)
+                            .map(item => ({ answer: item.answer, rank: item.rank }));
+
+            console.log(ans)
+            const myAnswer = "Answer 2";
+
+            const ans = myAnswer
+            const rank = answerAndRank.find(x => x.answer == myAnswer).rank;
+
+
+
+
+
+
+
+
+            setState({
+                question: qus,
+                answers: ans,
+                rank: rank,
+                loading: false
+            });
+
+
+
+
+        }
+        populateData();
+    }, []);
+
+    const { loading , question, answers, rank} = state;
+    const contents = loading ? <div>Loading...</div>
+        : <div>Question: {question}?
+            <br /> Answer(s): {answers}
+            <br /> Rank(s): {rank}</div>;
+
+    return contents
 }
+
+export default Home;
